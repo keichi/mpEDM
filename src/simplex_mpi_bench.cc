2f64d5fc59210998040b8ad9c35188f9f2f67a21
Keichi Takahashi <keichi.t@me.com>
2019-12-10 11:13:20 +0900

    Add MPI-parallelzed Simplex projection benchmark

---
+++ src/simplex_mpi_bench.cc
@@ -0,0 +1,100 @@
+#include <iostream>
+
+#include "dataset.h"
+#include "mpi_master.h"
+#include "mpi_worker.h"
+#include "simplex.h"
+#include "simplex_cpu.h"
+
+class SimplexMPIMaster : public MPIMaster
+{
+public:
+    SimplexMPIMaster(const std::string fname, MPI_Comm comm)
+        : MPIMaster(comm), dataset(fname)
+    {
+    }
+    ~SimplexMPIMaster() {}
+
+protected:
+    Dataset dataset;
+    int current_id;
+
+    void next_task(nlohmann::json &task)
+    {
+        task["id"] = current_id;
+        current_id++;
+    }
+
+    bool task_left() const { return current_id < dataset.timeseries.size(); }
+
+    void task_done(const nlohmann::json &result)
+    {
+        std::cout << "Timeseries #" << result["id"] << " best E=" << result["E"]
+                  << " rho=" << result["rho"] << std::endl;
+    }
+};
+
+class SimplexMPIWorker : public MPIWorker
+{
+public:
+    SimplexMPIWorker(const std::string fname, MPI_Comm comm)
+        : MPIWorker(comm), dataset(fname), simplex(1, 30, 1, true)
+    {
+    }
+    ~SimplexMPIWorker() {}
+
+protected:
+    Dataset dataset;
+    SimplexCPU simplex;
+
+    void do_task(nlohmann::json &result, const nlohmann::json &task)
+    {
+        const auto id = task["id"];
+
+        Timeseries ts = dataset.timeseries[id];
+
+        // Split input into two halves
+        Timeseries library(ts.data(), ts.size() / 2);
+        Timeseries predictee(ts.data() + ts.size() / 2, ts.size() / 2);
+
+        std::vector<float> rhos;
+
+        for (auto E = 1; E <= 20; E++) {
+            const auto rho = simplex.predict(library, predictee, E);
+            rhos.push_back(rho);
+        }
+
+        const auto it = std::max_element(rhos.begin(), rhos.end());
+        const auto maxE = it - rhos.begin() + 1;
+        const auto maxRho = *it;
+
+        result["id"] = id;
+        result["E"] = maxE;
+        result["rho"] = maxRho;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    MPI_Init(&argc, &argv);
+
+    if (argc < 2) {
+        std::cerr << "No input" << std::endl;
+        return -1;
+    }
+
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (!rank) {
+        SimplexMPIMaster master(argv[1], MPI_COMM_WORLD);
+
+        master.run();
+    } else {
+        SimplexMPIWorker worker(argv[1], MPI_COMM_WORLD);
+
+        worker.run();
+    }
+
+    MPI_Finalize();
+}
