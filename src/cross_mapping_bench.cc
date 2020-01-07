40754bf2622fdeaba011435ef3e7159386bcd406
Wassapon Watanakeesuntorn <wassapon.w@gmail.com>
2020-01-07 16:17:53 +0900

    Add NearestNeighborsGPU and SimplexGPU on cross_mapping_bench

--- src/cross_mapping_bench.cc
+++ src/cross_mapping_bench.cc
@@ -1,21 +1,29 @@
 #include <algorithm>
 #include <iostream>
 
+#include <argh.h>
+#ifdef ENABLE_GPU_KERNEL
+#include <arrayfire.h>
+#endif
+
 #include "cross_mapping_cpu.h"
 #include "dataset.h"
 #include "lut.h"
 #include "nearest_neighbors_cpu.h"
 #include "simplex_cpu.h"
+#ifdef ENABLE_GPU_KERNEL
+#include "../src/nearest_neighbors_gpu.h"
+#include "../src/simplex_gpu.h"
+#endif
 #include "stats.h"
 #include "timer.h"
 
-void simplex_projection(std::vector<uint32_t> &optmal_E, const Dataset &ds)
+template <class T, class U> void simplex_projection(std::vector<uint32_t> &optmal_E, const Dataset &ds)
 {
     LUT lut;
-    // tau=1, Tp=1, verbose=true
-    NearestNeighborsCPU knn(1, 1, true);
-    // tau=1, Tp=1, verbose=true
-    SimplexCPU simplex(1, 1, true);
+
+    auto knn = std::unique_ptr<NearestNeighbors>(new T(1, 1, true));
+    auto simplex = std::unique_ptr<Simplex>(new U(1, 1, true));
 
     optmal_E.resize(ds.n_cols());
 
@@ -31,11 +39,11 @@ void simplex_projection(std::vector<uint32_t> &optmal_E, const Dataset &ds)
         std::vector<float> buffer;
 
         for (auto E = 1; E <= 20; E++) {
-            knn.compute_lut(lut, library, target, E, E + 1);
+            knn->compute_lut(lut, library, target, E, E + 1);
             lut.normalize();
 
-            simplex.predict(prediction, buffer, lut, library, E);
-            simplex.shift_target(shifted_target, target, E);
+            simplex->predict(prediction, buffer, lut, library, E);
+            simplex->shift_target(shifted_target, target, E);
 
             const float rho = corrcoef(prediction, shifted_target);
 
@@ -62,9 +70,53 @@ void cross_mapping(const Dataset &ds, const std::vector<uint32_t> &optimal_E)
     xmap.run(rhos, ds, optimal_E);
 }
 
+void usage(const std::string &app_name)
+{
+    std::string msg =
+        app_name +
+        ": Cross Mapping Benchmark\n"
+        "\n"
+        "Usage:\n"
+        "  " +
+        app_name +
+        " [OPTION...] FILE\n"
+        "  -t, --tau arg    Lag (default: 1)\n"
+        "  -e, --emax arg   Maximum embedding dimension (default: 20)\n"
+        "  -p, --Tp arg     Steps to predict in future (default: 1)\n"
+        "  -x, --kernel arg Kernel type {cpu|gpu|multigpu} (default: cpu)\n"
+        "  -v, --verbose    Enable verbose logging (default: false)\n"
+        "  -h, --help       Show help";
+
+    std::cout << msg << std::endl;
+}
+
 int main(int argc, char *argv[])
 {
-    const std::string fname(argv[1]);
+    argh::parser cmdl({"-t", "--tau", "-p", "--tp", "-e", "--emax", "-x",
+                       "--kernel", "-v", "--verbose"});
+    cmdl.parse(argc, argv);
+
+    if (cmdl[{"-h", "--help"}]) {
+        usage(cmdl[0]);
+        return 0;
+    }
+
+    if (!cmdl(1)) {
+        std::cerr << "No input file" << std::endl;
+        usage(cmdl[0]);
+        return 1;
+    }
+
+    std::string fname = cmdl[1];
+    uint32_t tau;
+    cmdl({"t", "tau"}, 1) >> tau;
+    uint32_t Tp;
+    cmdl({"p", "Tp"}, 1) >> Tp;
+    uint32_t E_max;
+    cmdl({"e", "emax"}, 20) >> E_max;
+    std::string kernel_type;
+    cmdl({"x", "kernel"}, "cpu") >> kernel_type;
+    bool verbose = cmdl[{"v", "verbose"}];
 
     Timer timer_tot, timer_io, timer_simplex, timer_xmap;
 
@@ -85,7 +137,22 @@ int main(int argc, char *argv[])
 
     timer_simplex.start();
 
-    simplex_projection(optimal_E, ds);
+if (kernel_type == "cpu") {
+        std::cout << "Using CPU Simplex kernel" << std::endl;
+
+        simplex_projection<NearestNeighborsCPU, SimplexCPU>(optimal_E, ds);
+    }
+#ifdef ENABLE_GPU_KERNEL
+    else if (kernel_type == "gpu") {
+        std::cout << "Using GPU Simplex kernel" << std::endl;
+
+        simplex_projection<NearestNeighborsGPU, SimplexGPU>(optimal_E, ds);
+    }
+#endif
+    else {
+        std::cerr << "Unknown kernel type " << kernel_type << std::endl;
+        return 1;
+    }
 
     timer_simplex.stop();
 
