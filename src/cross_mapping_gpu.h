48ca630ccd24af8cbff1d6b421271fb0ccf58e5f
Wassapon Watanakeesuntorn <wassapon.w@gmail.com>
2020-01-09 19:25:33 +0900

    Reimplementation of Simplex for Cross Mapping

--- src/cross_mapping_gpu.h
+++ src/cross_mapping_gpu.h
@@ -1,6 +1,7 @@
 #ifndef __CROSS_MAPPING_GPU_H__
 #define __CROSS_MAPPING_GPU_H__
 
+#include <arrayfire.h>
 #include <memory>
 
 #include <arrayfire.h>
@@ -13,10 +14,9 @@
 class CrossMappingGPU : public CrossMapping
 {
 public:
-    CrossMappingGPU(uint32_t max_E, uint32_t tau, uint32_t Tp, bool verbose)
-        : CrossMapping(max_E, tau, Tp, verbose),
-          knn(new NearestNeighborsGPU(tau, Tp, verbose)),
-          simplex(new SimplexCPU(tau, Tp, verbose)), luts(max_E)
+    CrossMappingGPU(uint32_t E_max, uint32_t tau, uint32_t Tp, bool verbose)
+        : CrossMapping(E_max, tau, Tp, verbose),
+          knn(new NearestNeighborsGPU(tau, Tp, verbose)), luts(E_max)
     {
         n_devs = af::getDeviceCount();
     }
@@ -24,13 +24,17 @@ public:
     void run(std::vector<float> &rhos, const Dataset &ds,
              const std::vector<uint32_t> &optimal_E) override;
 
-    void predict(std::vector<float> &rhos, const Timeseries &library,
-                 const std::vector<Timeseries> &targets,
-                 const std::vector<uint32_t> &optimal_E);
+    void predict(std::vector<float> &rhos, const Dataset &ds, af::array data,
+                 const uint32_t index, const std::vector<uint32_t> &optimal_E);
+
+    void simplex(af::array &prediction, const LUT &lut, const af::array &target,
+                 uint32_t E);
+
+    void shift_target(af::array &shifted_target, const af::array &target,
+                      uint32_t E);
 
 protected:
     std::unique_ptr<NearestNeighbors> knn;
-    std::unique_ptr<Simplex> simplex;
     std::vector<LUT> luts;
     uint32_t n_devs;
 };
