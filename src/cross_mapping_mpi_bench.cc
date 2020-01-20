45f94fc3884bbbd3b994154352d204fab35829cd
Wassapon Watanakeesuntorn <wassapon.w@gmail.com>
2020-01-20 17:29:13 +0900

    Apply GPU kernel for MPIWorker

--- src/cross_mapping_mpi_bench.cc
+++ src/cross_mapping_mpi_bench.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <argh.h>
+
 #include "cross_mapping_cpu.h"
 #include "dataframe.h"
 #include "embedding_dim_cpu.h"
@@ -44,19 +46,17 @@ protected:
         std::cout << "Timeseries #" << result["id"] << " best E=" << result["E"]
                   << std::endl;
 
-        // result["id"] is the ID of the column
-        // result["E"] is the optimal embedding dimension
-
         optimal_E[result["id"]] = result["E"];
     }
 };
 
-class EmbeddingDimMPIWorker : public MPIWorker
+template <class T> class EmbeddingDimMPIWorker : public MPIWorker
 {
 public:
-    EmbeddingDimMPIWorker(const DataFrame df, MPI_Comm comm)
-        : MPIWorker(comm), embedding_dim(new EmbeddingDimCPU(20, 1, 1, true)),
-          dataframe(df)
+    EmbeddingDimMPIWorker(const DataFrame df, bool verbose, MPI_Comm comm)
+        : MPIWorker(comm),
+          embedding_dim(std::unique_ptr<EmbeddingDim>(new T(20, 1, 1, true))),
+          dataframe(df), verbose(verbose)
     {
     }
     ~EmbeddingDimMPIWorker() {}
@@ -64,6 +64,7 @@ public:
 protected:
     std::unique_ptr<EmbeddingDim> embedding_dim;
     DataFrame dataframe;
+    bool verbose;
 
     void do_task(nlohmann::json &result, const nlohmann::json &task) override
     {
@@ -71,7 +72,7 @@ protected:
         const auto ts = dataframe.columns[id];
         const auto best_E = embedding_dim->run(ts);
 
-        if (true) {
+        if (verbose) {
             std::cout << "Find embedding dimension for column #" << id
                       << " done" << std::endl;
         }
@@ -112,13 +113,15 @@ protected:
     }
 };
 
-class CrossMappingMPIWorker : public MPIWorker
+template <class T> class CrossMappingMPIWorker : public MPIWorker
 {
 public:
     CrossMappingMPIWorker(const DataFrame df,
-                          const std::vector<uint32_t> optimal_E, MPI_Comm comm)
-        : MPIWorker(comm), xmap(new CrossMappingCPU(20, 1, 0, true)),
-          dataframe(df), optimal_E(optimal_E)
+                          const std::vector<uint32_t> optimal_E, bool verbose,
+                          MPI_Comm comm)
+        : MPIWorker(comm),
+          xmap(std::unique_ptr<CrossMapping>(new T(20, 1, 0, true))),
+          dataframe(df), optimal_E(optimal_E), verbose(verbose)
     {
     }
     ~CrossMappingMPIWorker() {}
@@ -127,6 +130,7 @@ protected:
     std::unique_ptr<CrossMapping> xmap;
     DataFrame dataframe;
     std::vector<uint32_t> optimal_E;
+    bool verbose;
 
     void do_task(nlohmann::json &result, const nlohmann::json &task) override
     {
@@ -139,14 +143,62 @@ protected:
 
         result["id"] = id;
 
-        // if (true) {
-        std::cout << "Cross mapping for column #" << id << " done" << std::endl;
-        // }
+        if (verbose) {
+            std::cout << "Cross mapping for column #" << id << " done"
+                      << std::endl;
+        }
     }
 };
 
+void usage(const std::string &app_name)
+{
+    std::string msg =
+        app_name +
+        ": Cross Mapping Benchmark\n"
+        "\n"
+        "Usage:\n"
+        "  " +
+        app_name +
+        " [OPTION...] INPUT OUTPUT\n"
+        "  -t, --tau arg    Lag (default: 1)\n"
+        "  -e, --maxe arg   Maximum embedding dimension (default: 20)\n"
+        "  -p, --Tp arg     Steps to predict in future (default: 1)\n"
+        "  -x, --kernel arg Kernel type {cpu|gpu} (default: cpu)\n"
+        "  -v, --verbose    Enable verbose logging (default: false)\n"
+        "  -h, --help       Show help";
+
+    std::cout << msg << std::endl;
+}
+
 int main(int argc, char *argv[])
 {
+    argh::parser cmdl({"-t", "--tau", "-p", "--tp", "-e", "--maxe", "-x",
+                       "--kernel", "-v", "--verbose"});
+    cmdl.parse(argc, argv);
+
+    if (cmdl[{"-h", "--help"}]) {
+        usage(cmdl[0]);
+        return 0;
+    }
+
+    if (!cmdl(1)) {
+        std::cerr << "No input file" << std::endl;
+        usage(cmdl[0]);
+        return 1;
+    }
+
+    std::string input_fname = cmdl[1];
+
+    uint32_t tau;
+    cmdl({"t", "tau"}, 1) >> tau;
+    uint32_t Tp;
+    cmdl({"p", "Tp"}, 1) >> Tp;
+    uint32_t max_E;
+    cmdl({"e", "maxe"}, 20) >> max_E;
+    std::string kernel_type;
+    cmdl({"x", "kernel"}, "cpu") >> kernel_type;
+    bool verbose = cmdl[{"v", "verbose"}];
+
     MPI_Init(&argc, &argv);
 
     if (argc < 2) {
@@ -165,6 +217,8 @@ int main(int argc, char *argv[])
     Timer timer;
 
     if (!rank) {
+        std::cout << "Input: " << input_fname << std::endl;
+
         EmbeddingDimMPIMaster embedding_dim_master(df, MPI_COMM_WORLD);
 
         Timer timer_embedding_dim;
@@ -179,16 +233,23 @@ int main(int argc, char *argv[])
         std::cout << "Processed optimal E in " << timer_embedding_dim.elapsed()
                   << " [ms]" << std::endl;
     } else {
-        EmbeddingDimMPIWorker embedding_dim_worker(df, MPI_COMM_WORLD);
+        if (kernel_type == "cpu") {
+            EmbeddingDimMPIWorker<EmbeddingDimCPU> embedding_dim_worker(
+                df, verbose, MPI_COMM_WORLD);
 
-        embedding_dim_worker.run();
+            embedding_dim_worker.run();
+        }
+#ifdef ENABLE_GPU_KERNEL
+        if (kernel_type == "gpu") {
+            EmbeddingDimMPIWorker<EmbeddingDimGPU> embedding_dim_worker(
+                df, verbose, MPI_COMM_WORLD);
+
+            embedding_dim_worker.run();
+        }
+#endif
     }
 
     MPI_Bcast(optimal_E.data(), optimal_E.size(), MPI_FLOAT, 0, MPI_COMM_WORLD);
-
-    // for (size_t i = 0; i < optimal_E.size(); i++) {
-    //     std::cout << i << "," << optimal_E[i] << std::endl;
-    // }
 
     if (!rank) {
         CrossMappingMPIMaster cross_mapping_master(df, MPI_COMM_WORLD);
@@ -204,10 +265,20 @@ int main(int argc, char *argv[])
         std::cout << "Processed dataset in " << timer.elapsed() << " [ms]"
                   << std::endl;
     } else {
-        CrossMappingMPIWorker cross_mapping_worker(df, optimal_E,
-                                                   MPI_COMM_WORLD);
+        if (kernel_type == "cpu") {
+            CrossMappingMPIWorker<CrossMappingCPU> cross_mapping_worker(
+                df, optimal_E, verbose, MPI_COMM_WORLD);
 
-        cross_mapping_worker.run();
+            cross_mapping_worker.run();
+        }
+#ifdef ENABLE_GPU_KERNEL
+        if (kernel_type == "gpu") {
+            CrossMappingMPIWorker<CrossMappingGPU> cross_mapping_worker(
+                df, optimal_E, verbose, MPI_COMM_WORLD);
+
+            cross_mapping_worker.run();
+        }
+#endif
     }
 
     MPI_Finalize();
