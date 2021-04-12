114d1fc74b8f138c90ab89f6ca5829d6e53413d0
Wassapon Watanakeesuntorn <wassapon.w@gmail.com>
2021-04-12 17:39:05 +0900

    Revised the script

--- scripts/h52csv.py
+++ scripts/h52csv.py
@@ -8,8 +8,7 @@ import sys
 from pathlib import Path
 
 path = Path(sys.argv[1])
-df = h5py.File(path, 'r')
+file = h5py.File(path, 'r')
 
-for key in list(df):
-    data = pd.DataFrame(list(df[key]))
-    data.to_csv(path.with_suffix("." + key +".csv"), index = False)
+for key, data in file.items():
+    pd.DataFrame(data).to_csv(path.with_suffix("." + key + ".csv"), index = False)
